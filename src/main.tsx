fca2d597d6e8c4b763db7e9b9ece9d2bd369c0bf
Sarthak Bansal <gamingopgamingop67@gmail.com>
2025-10-03 21:52:04 +0530

    updated app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools , } from '@tanstack/react-query-devtools'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
