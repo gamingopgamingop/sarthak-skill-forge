c19e96291c7cde7c897b9670c5ab8ac0e62b405b
Sarthak Bansal <gamingopgamingop67@gmail.com>
2025-10-03 22:43:16 +0530

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRouter } from '@tanstack/react-router'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter , RouterProvider} from 'react-router-dom'
@@ -25,6 +25,7 @@ createRoot(document.getElementById('root')!).render(
       <HelmetProvider>
         <BrowserRouter>
           <RouterProvider router={routes} />
+          <SpeedInsights />
         </BrowserRouter>  
         <ReactQueryDevtools initialIsOpen={false} />
       </HelmetProvider>
