cc83e8638d4b569960ac505bc2d8324cde94ad43
Sarthak Bansal <gamingopgamingop67@gmail.com>
2025-10-03 22:07:50 +0530

    updated app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter , RouterProvider} from 'react-router-dom'
 import './index.css'
+import routes from './routes.tsx'
 import App from './App.tsx'
 
 const queryClient = new QueryClient({
@@ -21,8 +22,8 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <HelmetProvider>
         <BrowserRouter>
-          <App />
-        </BrowserRouter>
+          <RouterProvider router={routes} />
+        </BrowserRouter>  
         <ReactQueryDevtools initialIsOpen={false} />
       </HelmetProvider>
     </QueryClientProvider>
