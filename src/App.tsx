9ab6f8cb7638aba124633bb025fe90f2341bdc15
Sarthak Bansal <gamingopgamingop67@gmail.com>
2025-10-05 13:32:56 +0530

    add more pages and fx them

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import ContactPage from "@/pages/ContactPage";
 import TestimonialsPage from "@/pages/TestimonialsPage";
 import ServicesPage from "@/pages/ServicesPage";
 import GalleryPage from "@/pages/GalleryPage";
-import NotFound from "@/pages/NotFound";
+import NotFound from "@/pages/errors/NotFound";
 
 const App = () => {
   const location = useLocation();
