7dca5b33ce871638b2e7e76d915a2283142e1391
Sarthak Bansal <gamingopgamingop67@gmail.com>
2025-10-05 16:17:04 +0530

    update the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import reactBabel from '@vitejs/plugin-react';
