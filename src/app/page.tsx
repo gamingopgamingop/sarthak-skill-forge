bb966ceb5c289a84813c634ce519ed31797f9879
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-05 23:11:31 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+// export default function Page() {
+//   return <h1>Hello, Next.js!</h1>
+// }
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const filters = (await searchParams).filters
 }
