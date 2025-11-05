a4f48517881bf389c1351784614d6809d6d5d44e
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-05 23:37:30 +0530

    updated

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,14 @@
+// export async function getData() {
+//   const res = await fetch('https://external-service.com/data', {
+//     headers: {
+//       authorization: process.env.API_KEY,
+//     },
+//   })
+ 
+//   return res.json()
+// }
+import 'server-only'
+ 
 export async function getData() {
   const res = await fetch('https://external-service.com/data', {
     headers: {
