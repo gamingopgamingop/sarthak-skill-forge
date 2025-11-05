2c63aefdf2266e0fe98f319aee35a0dce2f063c1
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-05 23:29:53 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
+
+// Client Component
+import Search from './search'
+// Server Component
+import Logo from './logo'
+ 
+// Layout is a Server Component by default
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <nav>
+        <Logo />
+        <Search />
+      </nav>
+      <main>{children}</main>
+    </>
   )
 }
