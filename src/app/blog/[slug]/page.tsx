cb60d8a7e582fd8b044dad1b3312e0c7e4aae877
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-05 23:13:31 +0530

    updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,10 @@
-function generateStaticParams() {}
+// function generateStaticParams() {}
  
-export default function Page() {
-  return <h1>Hello, Blog Post Page!</h1>
+// export default function Page() {
+//   return <h1>Hello, Blog Post Page!</h1>
+// }
+
+export default async function Page(props: PageProps<'/blog/[slug]'>) {
+  const { slug } = await props.params
+  return <h1>Blog post: {slug}</h1>
 }
