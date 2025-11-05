b9a6f471348cb90974391a306c6de93823ca4394
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-05 23:32:30 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,21 @@
 //   return <h1>Hello, Next.js!</h1>
 // }
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-  const filters = (await searchParams).filters
+// export default async function Page({
+//   searchParams,
+// }: {
+//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+// }) {
+//   const filters = (await searchParams).filters
+// }
+
+import Modal from './ui/modal'
+import Cart from './ui/cart'
+ 
+export default function Page() {
+  return (
+    <Modal>
+      <Cart />
+    </Modal>
+  )
 }
