b925037f62567a136768fc2bebe493b88e6a68ee
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-06 18:59:51 +0530

    updated

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,8 @@
 // Dummy imports
 import { getPosts } from '@/lib/posts'
 import { Post } from '@/ui/post'
- 
+import { db, posts } from '@/lib/db'
+
 export default async function Page() {
   const posts = await getPosts()
  
@@ -9,6 +10,29 @@ export default async function Page() {
     <ul>
       {posts.map((post) => (
         <Post key={post.id} post={post} />
+      ))}
+    </ul>
+  )
+}
+
+export default async function Page() {
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default async function Page() {
+  const allPosts = await db.select().from(posts)
+  return (
+    <ul>
+      {allPosts.map((post) => (
+        <li key={post.id}>{post.title}</li>
       ))}
     </ul>
   )
