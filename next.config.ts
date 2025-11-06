6a334018c85341f40f474be3fc8a7fe7b846729c
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-06 19:02:58 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
- 
+
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  compiler: {
+    styledComponents: true,
+  },
 }
- 
+
 export default nextConfig
