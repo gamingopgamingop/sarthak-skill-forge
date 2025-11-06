8ee9412fa6bf44bd2f9a6343b466b258004c2425
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-06 19:35:10 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,32 @@
 import ThemeProvider from './theme-provider'
 import './global.css'
 import { Geist , Roboto} from 'next/font/google'
+import localFont from 'next/font/local'
+
+const roboto = localFont({
+  src: [
+    {
+      path: './Roboto-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './Roboto-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './Roboto-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './Roboto-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+})
 
 export default function RootLayout({
   children,
@@ -91,4 +117,21 @@ export default function RootLayout({
     </html>
   )
 }
+
+const myFont = localFont({
+  src: './my-font.woff2',
+})
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={myFont.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
+If y
 
