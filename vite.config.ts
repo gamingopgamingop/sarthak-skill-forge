40d2494be02539181cd5fafe11379b6e7a52731a
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-10 20:54:10 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig(({ mode }) => ({
     tsConfigPaths(),
     tanstackStart(),
     viteReact(),
-    reactStart()
+    reactStart(),
 
 
     mode === "development" && componentTagger(),
