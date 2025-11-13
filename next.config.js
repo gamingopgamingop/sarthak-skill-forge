49fdaea716efcf2768914a46cac7641538a2c0ce
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-13 21:32:24 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 // @ts-check
- 
+ const { withBlitz } = require("@blitzjs/next")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+    typedRoutes: true,
+  },
+
   /* config options here */
 }
  
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next'
+const { withBlitz } = require("@blitzjs/next")
 
 const nextConfig: NextConfig = {
+    experimental: {
+    typedRoutes: true,
+  },
+
   cacheComponents: true,
   
   compiler: {
