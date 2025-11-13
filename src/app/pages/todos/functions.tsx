b12f283d5e29740b09aa892252b54310763578a1
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-13 21:05:08 +0530

    updated

---
+++ src/app/pages/todos/functions.tsx
@@ -0,0 +1,9 @@
+"use server";
+
+import { requestInfo } from "rwsdk/worker";
+
+export async function addTodo(formData: FormData) {
+  const { ctx } = requestInfo;
+  const title = formData.get("title");
+  await db.todo.create({ data: { title, userId: ctx.user.id } });
+}
