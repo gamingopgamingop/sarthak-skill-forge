6d6ac81bfadcb6930ffc4272e71993bd60363222
gaming op gaming op <gamingopgamingop67@gmail.com>
2025-11-23 10:02:24 +0530

    updated

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
-const nextJest = require('next/jest')
- 
-// Providing the path to your Next.js app which will enable loading next.config.js and .env files
-const createJestConfig = nextJest({ dir: './' })
- 
-// Any custom config you want to pass to Jest
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
- 
-// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
-module.exports = createJestConfig(customJestConfig)
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+export default createJestConfig(customJestConfig);
