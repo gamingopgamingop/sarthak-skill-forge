6251a8d4e059e66a06e9e4af18a3f7f1683897ca
GamingOpgamingOp <sarthak.bansal_bca24@gla.ac.in>
2025-12-11 22:59:09 +0530

    chore(eslint): enhance config with import sorting and expanded ignores
    - Added eslint-plugin-simple-import-sort to enforce import and export sorting
    - Included additional ignore patterns for __snapshots__, coverage, and node_modules
    - Merged globals for browser and node environments
    - Adjusted TypeScript ESLint rules to relax some restrictions
    - Integrated react-hooks and react-refresh plugin configurations
    - Set ECMAScript version to 2020 for language options

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,38 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import sortImportPlugin from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist" , "__snapshots__", "coverage", "node_modules"] },
+    eslint.configs.recommended,
+  ...tslint.configs.recommended,
+
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "simple-import-sort": sortImportPlugin,
+
     },
     rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
