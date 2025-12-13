9130d2d70dc66b0b303b821a4485603b28fdc239
GamingOpgamingOp <sarthak.bansal_bca24@gla.ac.in>
2025-12-13 22:05:18 +0530

    chore(storybook): enable storyStoreV7 and add Marko and Remix React Router addons
    - Enabled storyStoreV7 feature flag for improved story management
    - Added "@storybook/addon-marko" to support Marko components
    - Included "storybook-addon-remix-react-router" for Remix React Router integration
    - Updated addons configuration asynchronously to append new addons
    - Maintained existing telemetry and notification disabling settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,21 @@
 import type { StorybookConfig } from "@storybook/marko-vite";
-
+import { marko } from "@storybook/addon-marko";
+import { angular } from "@storybook/addon-angular";
+import { remix } from "storybook-addon-remix-react-router";
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 export default {
   framework: "@storybook/marko-vite",
   stories: ["../src/**/{,*.}stories.ts"],
   core: {
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  async addons(entry = []) {
+    return [...entry, "@storybook/addon-marko","storybook-addon-remix-react-router" 
+    ];
   },
 } satisfies StorybookConfig;
 
