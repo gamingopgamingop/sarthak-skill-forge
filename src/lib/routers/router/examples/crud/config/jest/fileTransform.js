2d294dd094f7c52682706d8cf4d15ee736f8386c
gamingopgamingop <gamingopgamingop67@gmail.com>
2026-01-29 18:17:56 +0530

    refactor(jest): replace require with import in fileTransform
    Convert CommonJS require to ES6 import for consistency with modern module syntax. Also simplify comments and clarify the transform's purpose inline.

--- src/lib/routers/router/examples/crud/config/jest/fileTransform.js
+++ src/lib/routers/router/examples/crud/config/jest/fileTransform.js
@@ -1,12 +1,10 @@
 "use strict";
 
-const path = require("path");
-
-// This is a custom Jest transformer turning file imports into filenames.
-// http://facebook.github.io/jest/docs/en/webpack.html
+import path from 'path';
 
 module.exports = {
   process(src, filename) {
+    // Takes the full file path and returns just the filename as a JS module string
     return `module.exports = ${JSON.stringify(path.basename(filename))};`;
   }
 };
